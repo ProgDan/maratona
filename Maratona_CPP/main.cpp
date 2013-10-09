63c781833a086f532dc5080afb392227ed99cd7c
Daniel Arndt Alves <progdan@088245-cead-2.local>
2013-10-09 09:44:41 -0300

    Criação do Projeto em Linguagem C++

---
+++ Maratona_CPP/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: progdan
+ *
+ * Created on October 9, 2013, 9:44 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
