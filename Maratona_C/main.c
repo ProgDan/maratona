ad6b38897cd482c210f553fb8abfb8f91de943ca
Daniel Arndt Alves <progdan@088245-cead-2.local>
2013-10-09 09:45:42 -0300

    Criação do Projeto em Linguagem C

---
+++ Maratona_C/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: progdan
+ *
+ * Created on October 9, 2013, 9:45 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
