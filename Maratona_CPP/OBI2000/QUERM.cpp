5f1b0355e1c34c0a79aa8fd76cd89d96694a5725
Daniel Arndt Alves <progdan@088245-cead-2.local>
2013-10-09 10:45:36 -0300

    Problema QUERM - OBI2000

---
+++ Maratona_CPP/OBI2000/QUERM.cpp
@@ -0,0 +1,47 @@
+/* 
+ * File:   QUERM.cpp
+ * Author: progdan
+ * 
+ * Solucao para o problema "Quermesse" da OBI2000
+ *
+ * Created on October 9, 2013, 10:40 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+  int n = 1;
+  while (true){
+      // lê número de participantes da festa
+      int N;
+      cin >> N;
+      if (!N) break;
+
+      // lê entrada e encontra solução
+      int ganhador = -1;
+      int posicao = 1;
+      while (posicao <= N)
+        {
+          int ingresso;
+          cin >> ingresso;
+          if (posicao == ingresso) 
+            {
+              if (ganhador != -1)
+                cerr << "*** + de uma solução" << endl;
+              else
+                ganhador = posicao;
+            }
+          posicao++;
+        }
+
+      // Exibe a Solução
+      cout << "Teste " << n++ << endl;
+      cout << ganhador << endl;
+      cout << endl;
+    }
+
+    return 0;
+}
